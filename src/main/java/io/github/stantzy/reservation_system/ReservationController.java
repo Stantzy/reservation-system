e10147282e8e050c4231680e7a123191b5f3ba2d
Igor Bazhev <bazhew@mail.ru>
2025-09-30 17:35:46 +0500

    upd

--- src/main/java/io/github/stantzy/reservation_system/ReservationController.java
+++ src/main/java/io/github/stantzy/reservation_system/ReservationController.java
@@ -2,15 +2,18 @@ package io.github.stantzy.reservation_system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /* класс обработки HTTP-запросов */
 
 @RestController
+/* ко всем путям в маппингах будет добавлена эта строка из @RequestMapping */
+@RequestMapping("/reservation")
 public class ReservationController {
     /* логгирование */
     private static final Logger log =
@@ -23,16 +26,63 @@ public class ReservationController {
     }
 
     @GetMapping("/{id}")
-    public Reservation getReservationById(
+    public ResponseEntity<Reservation> getReservationById(
             @PathVariable("id")Long id
     ) {
         log.info("Called getReservationById: id=" + id);
-        return reservationService.getReservationById(id);
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(reservationService.getReservationById(id));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @GetMapping
-    public List<Reservation> getAllReservations() {
+    public ResponseEntity<List<Reservation>> getAllReservations() {
         log.info("Called getAllReservations");
-        return reservationService.findAllReservations();
+        return ResponseEntity.ok(reservationService.findAllReservations());
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(
+            @RequestBody Reservation reservationToCreate
+    ) {
+        log.info("Called method createReservation");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(reservationService.createReservation(reservationToCreate));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Reservation> updateReservation(
+            @PathVariable Long id,
+            @RequestBody Reservation reservationToUpdate
+    ) {
+        log.info("Called method updateReservation: id={}, reservationToUpdate={}",
+                id, reservationToUpdate);
+        var updated = reservationService.updateReservation(id, reservationToUpdate);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(
+            @PathVariable Long id
+    ) {
+        log.info("Called method deleteReservation: id={}", id);
+        try {
+            reservationService.deleteReservation(id);
+            return ResponseEntity.ok().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping("/{id}/approve")
+    public ResponseEntity<Reservation> approveReservation(
+            @PathVariable Long id
+    ) {
+        log.info("Called method approveReservation: id={}", id);
+        var reservation = reservationService.approveReservation(id);
+        return ResponseEntity.ok(reservation);
     }
 }
--- src/main/java/io/github/stantzy/reservation_system/ReservationService.java
+++ src/main/java/io/github/stantzy/reservation_system/ReservationService.java
@@ -2,41 +2,29 @@ package io.github.stantzy.reservation_system;
 
 /* класс обработки бизнес-логики */
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ReservationService {
-    private final Map<Long, Reservation> reservationMap = Map.of(
-        1L, new Reservation(
-                    1L,
-                    100L,
-                    40L,
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(5),
-                    ReservationStatus.APPROVED
-        ),
-        2L, new Reservation(
-                    2L,
-                    102L,
-                    41L,
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(5),
-                    ReservationStatus.APPROVED
-        ),
-        3L, new Reservation(
-                3L,
-                103L,
-                42L,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5),
-                ReservationStatus.APPROVED
-        )
-    );
+    private final Map<Long, Reservation> reservationMap;
+    private final AtomicLong idCounter;
+
+    public ReservationService() {
+        reservationMap = new HashMap<>();
+        idCounter = new AtomicLong();
+    }
 
     public Reservation getReservationById(Long id) {
         if(!reservationMap.containsKey(id)) {
@@ -47,5 +35,104 @@ public class ReservationService {
 
     public List<Reservation> findAllReservations() {
         return reservationMap.values().stream().toList();
+    }
+
+    public Reservation createReservation(Reservation reservationToCreate) {
+        if(reservationToCreate.id() != null)
+            throw new IllegalArgumentException("Id should be empty");
+        if(reservationToCreate.status() != null)
+            throw new IllegalArgumentException("Status should be empty");
+
+        var newReservation = new Reservation(
+            idCounter.incrementAndGet(),
+            reservationToCreate.userId(),
+            reservationToCreate.roomId(),
+            reservationToCreate.startDate(),
+            reservationToCreate.endDate(),
+            ReservationStatus.PENDING
+        );
+        reservationMap.put(newReservation.id(), newReservation);
+        return newReservation;
+    }
+
+    public Reservation updateReservation(
+            Long id,
+            Reservation reservationToUpdate
+    ) {
+        if(!reservationMap.containsKey(id))
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+
+        var reservation = reservationMap.get(id);
+        if(reservation.status() != ReservationStatus.PENDING) {
+            throw new IllegalStateException(
+                    "Cannot modify reservation: status=" + reservation.status()
+            );
+        }
+
+        var updatedReservation = new Reservation(
+                reservation.id(),
+                reservationToUpdate.userId(),
+                reservationToUpdate.roomId(),
+                reservationToUpdate.startDate(),
+                reservationToUpdate.endDate(),
+                ReservationStatus.PENDING
+        );
+        reservationMap.put(updatedReservation.id(), updatedReservation);
+        return updatedReservation;
+    }
+
+    public void deleteReservation(Long id) {
+        if(!reservationMap.containsKey(id))
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        reservationMap.remove(id);
+    }
+
+    public Reservation approveReservation(Long id) {
+        if(!reservationMap.containsKey(id))
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+
+        var reservation = reservationMap.get(id);
+        if(reservation.status() != ReservationStatus.PENDING) {
+            throw new IllegalStateException(
+                    "Cannot approve reservation: status=" + reservation.status()
+            );
+        }
+
+        var isConflict = isReservationConflict(reservation);
+        if(isConflict) {
+            throw new IllegalStateException(
+                    "Cannot approve reservation because of conflict"
+            );
+        }
+
+        var approvedReservation = new Reservation(
+                reservation.id(),
+                reservation.userId(),
+                reservation.roomId(),
+                reservation.startDate(),
+                reservation.endDate(),
+                ReservationStatus.APPROVED
+        );
+        reservationMap.put(approvedReservation.id(), approvedReservation);
+        return approvedReservation;
+    }
+
+    private boolean isReservationConflict(Reservation reservation) {
+        for(Reservation existingReservation : reservationMap.values()) {
+            if(reservation.id().equals(existingReservation.id())) {
+                continue;
+            }
+            if(!reservation.roomId().equals(existingReservation.roomId())) {
+                continue;
+            }
+            if(!existingReservation.status().equals(ReservationStatus.APPROVED)) {
+                continue;
+            }
+            if(reservation.startDate().isBefore(existingReservation.endDate())
+                && existingReservation.startDate().isBefore(reservation.endDate())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
