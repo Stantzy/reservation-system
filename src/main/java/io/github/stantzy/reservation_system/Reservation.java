9dce5322652a1ee70cd7bc8c516e59bdad9ed380
Igor Bazhev <bazhew@mail.ru>
2025-09-30 12:51:39 +0500

    minimal functionality added

---
+++ src/main/java/io/github/stantzy/reservation_system/Reservation.java
@@ -0,0 +1,13 @@
+package io.github.stantzy.reservation_system;
+
+import java.time.LocalDate;
+
+public record Reservation(
+        Long id,
+        Long userId,
+        Long roomId,
+        LocalDate startDate,
+        LocalDate endDate,
+        ReservationStatus status
+) {
+}
---
+++ src/main/java/io/github/stantzy/reservation_system/ReservationStatus.java
@@ -0,0 +1,7 @@
+package io.github.stantzy.reservation_system;
+
+public enum ReservationStatus {
+    PENDING,
+    APPROVED,
+    CANCELLED
+}
