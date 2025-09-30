31287d413996554c0901e67c79c421366bfae3b7
Igor Bazhev <bazhew@mail.ru>
2025-09-30 12:12:51 +0500

    Initial commit

---
+++ src/test/java/io/github/stantzy/reservation_system/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.stantzy.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
